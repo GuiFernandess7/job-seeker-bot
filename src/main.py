d011500be8158271d7b1bca4b81cb5bc1ad5bf38
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-11-08 22:41:13 -0300

    feat: Builder design pattern added

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -12,17 +15,77 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import logging
 
-class JobScraper:
-
+class RPAProduct:
     def __init__(self):
         self.driver = None
-        self.logger = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.INFO)
+        self.search_results = []
 
-    def set_chrome_options(self):
-        """Chrome configuration settings"""
+    def set_search_results(self, results):
+        self.search_results = results
+
+    def get_search_results(self):
+        return self.search_results
+
+class RPABuilder(ABC):
+    @property
+    @abstractmethod
+    def product(self) -> RPAProduct:
+        pass
+
+    @abstractmethod
+    def set_driver(self, browser: str) -> RPABuilder:
+        pass
+
+    @abstractmethod
+    def set_chrome_options(self) -> None:
+        pass
+
+    @abstractmethod
+    def open_url(self, url: str) -> RPABuilder:
+        pass
+
+    @abstractmethod
+    def find_search_elements(self) -> RPABuilder:
+        pass
+
+    @abstractmethod
+    def apply_search(self, query: str) -> RPABuilder:
+        pass
+
+    @abstractmethod
+    def extract_websites(self) -> RPABuilder:
+        pass
+
+    @abstractmethod
+    def driver_quit(self) -> None:
+        pass
+
+class RPAConcreteBuilder(RPABuilder):
+    def __init__(self):
+        self._driver = RPAProduct()
+
+    @property
+    def product(self) -> RPAProduct:
+        product = self._driver
+        self.reset()
+        return product
+
+    def reset(self) -> None:
+        self._driver = RPAProduct()
+
+    def set_driver(self, browser: str = "Chrome") -> RPABuilder:
+        options = self.set_chrome_options()
+        if browser.lower() == "chrome":
+            self._driver.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        else:
+            raise ValueError("Unsupported browser")
+        return self
+
+    def set_chrome_options(self, headless: bool = False) -> None:
         options = Options()
-        options.add_argument("--headless")
+        if headless:
+            options.add_argument("--headless")
+
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
@@ -33,138 +96,80 @@ class JobScraper:
         options.add_argument("--window-size=1920,1080")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         options.binary_location = "/usr/bin/google-chrome"
-
         return options
 
-    def set_webdriver(self, browser="Chrome"):
-        options = self.set_chrome_options()
-        if browser.lower() == "chrome":
-            self.logger.warning("Using Chrome WebDriver.")
-            self.driver = webdriver.Chrome(
-                service=Service(ChromeDriverManager().install()), options=options
-            )
-        else:
-            self.logger.error("Unsupported browser. Only Chrome is supported.")
-            raise ValueError("Unsupported browser")
+    def open_url(self, url: str) -> RPABuilder:
+        self._driver.driver.get(url)
+        return self
 
-    def __check_driver(self):
-        if not self.driver:
-            self.logger.error("Driver not initialized.")
-            raise RuntimeError("Driver not initialized.")
+    def find_search_elements(self) -> RPABuilder:
+        self._driver.driver.find_element(By.NAME, "q")
+        return self
 
-    def open_url(self, url: str):
-        self.__check_driver()
-        try:
-            self.driver.get(url)
-            self.logger.info(f"Opened URL: {url}")
-        except Exception as e:
-            self.logger.error(f"Failed to open URL: {url} with error: {e}")
-            self.driver_quit()
-
-    def apply_search(self, site: str, position: str, mode: str = "remote"):
-        """Search for job postings and return results."""
-        self.__check_driver()
-        search_phrase = f"site:{site} '{position}' '{mode}'"
-
-        wait = WebDriverWait(self.driver, 10)
+    def apply_search(self, query: str) -> RPABuilder:
+        search_box = self._driver.driver.find_element(By.NAME, "q")
+        wait = WebDriverWait(self._driver.driver, 10)
         search_box = wait.until(
             EC.presence_of_element_located((By.NAME, "q"))
         )
-        search_box.send_keys(search_phrase)
+        search_box.send_keys(query)
         search_box.send_keys(Keys.RETURN)
-
         wait.until(
             EC.presence_of_element_located((By.ID, "search"))
         )
+        return self
 
-    def __find_search_elements(self):
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "g"))
-            )
-
-        except Exception as e:
-            print(f"Error waiting for elements: {e}")
-            self.driver_quit()
-
-    def __has_application_form(self, url) -> bool:
-        self.driver.get(url)
+    def extract_websites(self) -> RPABuilder:
+        searchResults = self._driver.driver.find_elements(By.CSS_SELECTOR, "h3.LC20lb")
 
         try:
-            forms = self.driver.find_elements(By.TAG_NAME, "form")
-            if forms:
-                return True
-
-        except Exception as e:
-            print(f"Error verifying form: {url} - {e}")
-
-        return False
-
-    @property
-    def pages(self):
-        pageData = []
-        self.__find_search_elements()
-
-        try:
-            searchResults = self.driver.find_elements(By.CSS_SELECTOR, "h3.LC20lb")
-            urls = []
-
             for result in searchResults:
                 parent = result.find_element(By.XPATH, "..")
                 url = parent.get_attribute("href")
-                urls.append(url)
                 text = result.text
-                pageData.append((text, url))
+                self._driver.search_results.append((text, url))
 
         except Exception as e:
             print(f"Error extracting results: {e}")
             self.driver_quit()
+        return self
 
-        return pageData
+    def driver_quit(self) -> None:
+        if self._driver.driver:
+            self._driver.driver.quit()
 
-    def apply(self, pageData):
-        for _, url in pageData:
-            has_form = self.__has_application_form(url)
+class JobSeekerBot:
 
-            if has_form:
-                elements = self.driver.find_elements(By.CLASS_NAME, "field")
+    def __init__(self, builder: RPABuilder) -> None:
+        self._builder = builder
 
-                for element in elements:
-                    try:
-                        input_element = element.find_element(By.XPATH, ".//input")
-                        print("Input found:", input_element.get_attribute('id'))
-                    except NoSuchElementException:
-                        logging.info(f"Input not found")
-                    else:
-                        input_element = element.find_element(By.TAG_NAME, "input")
-                        text = element.text
-                        if "*" in text:
-                            print(f"Field: {text}")
-                            print(f"input Name: {input_element.get_attribute('id')}")
-                            print('----------')
+    @property
+    def builder(self) -> RPABuilder:
+        return self._builder
 
-            break
+    def build_minimal_viable_product(self, url: str) -> None:
+        self.builder.set_driver().open_url(url)
 
-        return
+    def build_full_featured_product(self, url: str, search_query: str) -> None:
+        self.builder.set_driver()\
+            .open_url(url)\
+            .find_search_elements()\
+            .apply_search(search_query)\
+            .extract_websites()
 
-    def driver_quit(self):
-        if self.driver:
-            self.driver.quit()
-            self.logger.info("Driver quit successfully.")
-
-def run_news_task():
-    scraper = JobScraper()
-    scraper.set_webdriver()
-    scraper.open_url("https://www.google.com")
-    # site:https://jobs[.]lever[.]co "React Developer" "remote" -"remote only in the US"
-    scraper.apply_search(site="boards[.]greenhouse[.]io", position="Python Developer", mode="remote - remote only in the US")
-    results = scraper.pages
-    scraper.apply(results)
-
-    for result in results:
-        print(result)
-
-    scraper.driver_quit()
+def configure_logging():
+    logging.basicConfig(level=logging.INFO)
 
 if __name__=="__main__":
-    run_news_task()
+    configure_logging()
+    builder = RPAConcreteBuilder()
+    bot = JobSeekerBot(builder)
+
+    search_phrase = "React Developer" "remote" " - remote only in the US"
+    bot.build_full_featured_product('http://www.google.com', search_phrase)
+    search_results = builder.product.get_search_results()
+
+    for result in search_results:
+        print(result)
+
+    builder.driver_quit()
