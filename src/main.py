5dca57bdd9eff764be39943fc28f06d5c7baabcd
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-11-16 03:22:04 -0300

    feat: special queries added

--- src/main.py
+++ src/main.py
@@ -14,6 +14,11 @@ from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 
 import logging
+import time
+import random
+import pickle
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 class RPAProduct:
     def __init__(self):
@@ -74,11 +79,13 @@ class RPAConcreteBuilder(RPABuilder):
         self._driver = RPAProduct()
 
     def set_driver(self, browser: str = "Chrome") -> RPABuilder:
-        options = self.set_chrome_options()
+        options = self.set_chrome_options(headless=False)
         if browser.lower() == "chrome":
             self._driver.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         else:
             raise ValueError("Unsupported browser")
+
+        #self.load_cookies()
         return self
 
     def set_chrome_options(self, headless: bool = False) -> None:
@@ -86,6 +93,7 @@ class RPAConcreteBuilder(RPABuilder):
         if headless:
             options.add_argument("--headless")
 
+        #options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
@@ -100,6 +108,7 @@ class RPAConcreteBuilder(RPABuilder):
 
     def open_url(self, url: str) -> RPABuilder:
         self._driver.driver.get(url)
+        logging.info(f"Opened URL: {url}")
         return self
 
     def find_search_elements(self) -> RPABuilder:
@@ -114,9 +123,13 @@ class RPAConcreteBuilder(RPABuilder):
         )
         search_box.send_keys(query)
         search_box.send_keys(Keys.RETURN)
+
+        time.sleep(random.uniform(2, 5))
+
         wait.until(
             EC.presence_of_element_located((By.ID, "search"))
         )
+        logging.info(f"Search applied with query: {query}")
         return self
 
     def extract_websites(self) -> RPABuilder:
@@ -129,14 +142,41 @@ class RPAConcreteBuilder(RPABuilder):
                 text = result.text
                 self._driver.search_results.append((text, url))
 
+            logging.info(f"Extracted {len(searchResults)} results.")
         except Exception as e:
-            print(f"Error extracting results: {e}")
+            logging.error(f"Error extracting results: {e}")
             self.driver_quit()
         return self
 
     def driver_quit(self) -> None:
         if self._driver.driver:
             self._driver.driver.quit()
+            logging.info("Driver quit successfully.")
+
+    def is_captcha_present(self) -> bool:
+        """NOT IMPLEMENTED"""
+        try:
+            iframe = self._driver.driver.find_element(By.XPATH, "//iframe[contains(@src, 'recaptcha')]")
+            return True
+        except:
+            return False
+
+    def save_cookies(self, filename="cookies.pkl"):
+        """NOT IMPLEMENTED"""
+        cookies = self._driver.driver.get_cookies()
+        pickle.dump(cookies, open(filename, "wb"))
+        logging.info(f"Cookies saved successfully to {filename}.")
+
+    def load_cookies(self, filename="cookies.pkl"):
+        """NOT IMPLEMENTED"""
+        try:
+            self._driver.driver.get("https://www.google.com")
+            cookies = pickle.load(open(filename, "rb"))
+            for cookie in cookies:
+                self._driver.driver.add_cookie(cookie)
+            logging.info("Cookies loaded successfully.")
+        except FileNotFoundError:
+            logging.warning(f"Cookie file {filename} not found. Continuing without cookies.")
 
 class JobSeekerBot:
 
@@ -151,25 +191,52 @@ class JobSeekerBot:
         self.builder.set_driver().open_url(url)
 
     def build_full_featured_product(self, url: str, search_query: str) -> None:
-        self.builder.set_driver()\
-            .open_url(url)\
-            .find_search_elements()\
-            .apply_search(search_query)\
+        self.builder.set_driver().open_url(url)
+
+        """ if self.builder.is_captcha_present():
+            logging.info("CAPTCHA detected! Please solve it manually.")
+            captcha_resolved = False
+            start_time = time.time()
+            timeout = 120
+
+            while not captcha_resolved and time.time() - start_time < timeout:
+                if not self.builder.is_captcha_present():
+                    captcha_resolved = True
+                    logging.info("CAPTCHA solved! Proceeding with the search.")
+                else:
+                    logging.info("Waiting for CAPTCHA resolution...")
+                    time.sleep(5)
+
+            if not captcha_resolved:
+                logging.error("Time for CAPTCHA resolution expired. Try again later.")
+                return """
+
+        self.builder.apply_search(search_query)\
             .extract_websites()
 
 def configure_logging():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     configure_logging()
     builder = RPAConcreteBuilder()
     bot = JobSeekerBot(builder)
 
-    search_phrase = "Python Developer Jr"
-    bot.build_full_featured_product('http://www.google.com', search_phrase)
-    search_results = builder.product.get_search_results()
+    job_queries = [
+        "site:netvagas.com.br python junior developer",
+        "site:zapier.workable.com python junior developer remote"
+    ]
 
-    for result in search_results:
-        print(result)
+    try:
+        for query in job_queries:
+            bot.build_full_featured_product('http://www.google.com', query)
+            search_results = builder.product.get_search_results()
 
-    builder.driver_quit()
+            for result in search_results:
+                with open("output.txt", '+a') as f:
+                    f.write(str(result) + '\n')
+
+            logging.info("Search results saved to results.txt.")
+
+    finally:
+        builder.driver_quit()
